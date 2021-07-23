ad83758af462a8a04fd2caebac8613f1bb7a6088
Electroww <david.merwan@gmail.com>
2021-07-23 17:37:17 +0200

    add denormalize

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,18 +1,34 @@
 <template>
-  <ul class="tree">
-    <li class="folder" v-for="idFolder in tree" :key="idFolder">
-      {{ folders[idFolder].label }}
-      <ul class="areas">
-        <li
-          class="area"
-          v-for="idArea in folders[idFolder].children"
-          :key="idArea"
-        >
-          {{ areas[idArea].label }}
-        </li>
-      </ul>
-    </li>
-  </ul>
+  <div>
+    <ul class="tree">
+      <li class="folder" v-for="idFolder in tree" :key="idFolder">
+        {{ folders[idFolder].label }}
+        <input
+          type="text"
+          placeholder="renommer"
+          v-model="folders[idFolder].label"
+        />
+        <ul class="areas">
+          <li
+            class="area"
+            v-for="idArea in folders[idFolder].children"
+            :key="idArea"
+          >
+            {{ areas[idArea].label }}
+            <input
+              type="text"
+              placeholder="renommer"
+              v-model="areas[idArea].label"
+            />
+          </li>
+        </ul>
+      </li>
+      <button @click="$store.commit('addFolder')">Ajouter Dossier</button>
+    </ul>
+    <button @click="$store.commit('saveTree')">
+      Denormalize (see console.log)
+    </button>
+  </div>
 </template>
 
 <script>
@@ -20,7 +36,6 @@ export default {
   name: "Tree",
   mounted() {
     this.$store.commit("loadTree");
-    console.log(this.$store.state.tree);
   },
   computed: {
     areas() {
@@ -40,5 +55,9 @@ export default {
 <style scoped>
 .folder {
   font-weight: bold;
+  margin-bottom: 20px;
+}
+.areas {
+  margin-top: 10px;
 }
 </style>
--- src/store/main.js
+++ src/store/main.js
@@ -1,7 +1,7 @@
 
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { schema, normalize } from 'normalizr'
+import { schema, normalize, denormalize } from 'normalizr'
 
 import dataTree from "../data/tree"
 
@@ -11,7 +11,6 @@ const areaSchema = new schema.Entity('areas')
 const folderSchema = new schema.Entity('folders', {
     children: [areaSchema]
 })
-const treeSchema = new schema.Array(folderSchema)
 
 const tree = {
     state: {
@@ -21,15 +20,36 @@ const tree = {
     },
     mutations: {
         loadTree(state) {
-            const { entities: { areas, folders }, result } = normalize(dataTree, treeSchema)
+            const { entities: { areas, folders }, result } = normalize(dataTree, [folderSchema])
             state.tree = result
             state.folders = folders
             state.areas = areas
+        },
+        addFolder(state) {
+            const id = Object.keys(state.folders).length + 1
+            Vue.set(state.folders, id, {
+                id: id,
+                label: "Nouveau Dossier",
+                children: []
+            })
+            state.tree.push(id)
+        },
+        saveTree(state) {
+            const entities = (({ folders, areas }) => ({ folders, areas }))(state);
+            const denormalizedTree = denormalize(state.tree, [folderSchema], entities)
+            console.log("Données Dénormalisées :")
+            console.log(denormalizedTree)
         }
     },
     actions: {
         loadTree(context) {
             context.commit('loadTree')
+        },
+        addFolder(context) {
+            context.commit('addFolder')
+        },
+        saveTree(context) {
+            context.commit('saveTree')
         }
     }
 }
